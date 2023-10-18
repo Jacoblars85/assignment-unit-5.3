74b0f687862169f9b18f0dd7c707750a31c5cb45
Jacob Larson <hockeybobber@comcast.net>
2023-10-18 17:40:01 -0500

    Finished 2nd question.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,21 +5,67 @@ let myCollection = [];
 
 
 
-function addToCollection(Collection, title, artist, yearPublished) {
+function addToCollection(collection, title, artist, yearPublished) {
   let album ={
     title: title,
     artist: artist,
     yearPublished: yearPublished
     }
-    Collection.push(album);
+    collection.push(album);
     return album;
 }
 
 
 
+let album0 ={
+  title: 'Stoney',
+  artist: 'Post Malone',
+  yearPublished: 2016
+}
 
+console.log(addToCollection(myCollection, album0.title, album0.artist, album0.yearPublished));
 
+let album1 ={
+  title: 'Beerbongs & Bentleys',
+  artist: 'Post Malone',
+  yearPublished: 2018
+}
 
+console.log(addToCollection(myCollection, album1.title, album1.artist, album1.yearPublished));
+
+let album2 ={
+  title: 'Hollywoods Bleeding',
+  artist: 'Post Malone',
+  yearPublished: 2019
+}
+
+console.log(addToCollection(myCollection, album2.title, album2.artist, album2.yearPublished));
+
+let album3 ={
+  title: 'Twelve Carat Toothache',
+  artist: 'Post Malone',
+  yearPublished: 2022
+}
+
+console.log(addToCollection(myCollection, album3.title, album3.artist, album3.yearPublished));
+
+let album4 ={
+  title: 'Austin',
+  artist: 'Post Malone',
+  yearPublished: 2023
+}
+
+console.log(addToCollection(myCollection, album4.title, album4.artist, album4.yearPublished));
+
+let album5 ={
+  title: 'Legends Never Die',
+  artist: 'Juice WRLD',
+  yearPublished: 2020
+}
+
+console.log(addToCollection(myCollection, album5.title, album5.artist, album5.yearPublished));
+
+console.log('this is my collection:', myCollection);
 
 
 
